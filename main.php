0d55a777cf40a7fb91380f364e1c5ea4ee82090b
kissakalae <43167248+kissakalae@users.noreply.github.com>
2019-02-27 13:15:06 +0200

    4th SQL query

--- main.php
+++ main.php
@@ -30,12 +30,11 @@ Host information: 127.0.0.1:50361
     if ($result->num_rows > 0) {
         // output data of each row
         while($row = $result->fetch_assoc()) {
-            echo "id: " . $row["id"]. " - Name: " . $row["Name"]. "<br>";
+            echo "id: " . $row["asiakasid"]. " - Name: " . $row["sahkoposti"]. "<br>";
         }
     } else {
         echo "0 results";
     }
-	
 ?> 
 
 <meta charset="utf-8">
@@ -48,7 +47,7 @@ Host information: 127.0.0.1:50361
   <body>
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-      <a class="navbar-brand" href="#"><?php echo $results ?></a>
+      <a class="navbar-brand" href="#"><?php echo $result ?></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
