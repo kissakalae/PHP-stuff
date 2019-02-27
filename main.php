9cf14f0e40be2a5e13c6188090f0f058d9a5b0bd
kissakalae <43167248+kissakalae@users.noreply.github.com>
2019-02-27 13:08:01 +0200

    2nd SQL query

--- main.php
+++ main.php
@@ -24,6 +24,7 @@ Host information: 127.0.0.1:50361
 	}
 	
 	$sql = "SELECT * FROM asiakas;"
+	$result = $conn->query($sql);
 	
 ?> 
 
@@ -37,7 +38,7 @@ Host information: 127.0.0.1:50361
   <body>
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-      <a class="navbar-brand" href="#"><?php echo $sql ?></a>
+      <a class="navbar-brand" href="#"><?php echo $results ?></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
